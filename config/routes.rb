4c178b0c7c7c29e34b4436cdf1aa32112ef7cecb
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2014-10-16 09:17:36 +0400

    Deduplicated code with extended_fields

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-RedmineApp::Application.routes.draw do
-	resources :projects do
-		member do
-			get 'autocomplete_for_user',	:to => 'projects#autocomplete_for_user'
-		end
-	end
-	get 'issues/:id/autocomplete_for_user',		:to => 'issues#autocomplete_for_user'
-end
-
